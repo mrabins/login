26cc3a3b097a9d2d8ee0c6483fce22aa1ea0c8d0
mrabins <markrabins@gmail.com>
2015-08-25 21:54:59 -0700

    First commit

---
+++ LoginProject/CreateAccountViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateAccountViewController.swift
+//  LoginProject
+//
+//  Created by Mark Rabins on 8/25/15.
+//  Copyright (c) 2015 self.swift. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
